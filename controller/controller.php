2af136ac23c8125b43d2abdb423a8660c9a981f1
exphoenee <61277296+exphoenee@users.noreply.github.com>
2021-06-08 16:45:51 +0200

    Database errors is not handled yet

--- controller/controller.php
+++ controller/controller.php
@@ -2,18 +2,37 @@
 
 class Controller {
 
-  public static function getPage() {
-    $page = Request::Get("page", "home");
+  public static function renderPage() {
+    $page = Request::getPage();
 
     $html = '';
-    $html .= PageView::header();
-    $html .= PageView::body();
+    $html .= View::header();
+    $html .= View::openBody();
+    $html .= View::navbar();
+
+    switch ($page) {
+      case "home":
+      default:
+        $html .= HomeView::home();
+        break;
+      case "users":
+        UserController::hanldeUsers();
+        $users = UserController::read();
+        $html .= UserView::home($users);
+        break;
+      case "adverts":
+        $html .= AdvertView::home();
+        break;
+    }
+
+    $html .= View::closeBody();
     echo $html;
   }
 
   public static function create($table, $data) {
     $sql = Sql::Insert($table)->setFields($data);
-    return Database::Exec($sql, $data);
+    Database::Exec($sql);
+    return Database::Result();
   }
 
   public static function read($table, $id = null) {
@@ -21,20 +40,23 @@ class Controller {
     if ($id  !== null) {
       $sql->setWhere("`user_id` = ".$id);
     }
-    return Database::Exec($sql);
+    Database::Exec($sql);
+    return Database::Result();
   }
 
   public static function update($table, $id, $data) {
     $sql = Sql::Update($table)
       ->setFields($data)
       ->setWhere("`user_id` = ".$id);
-    return Database::Exec($sql);
+    Database::Exec($sql);
+    return Database::Result();
   }
 
   public static function delete($table, $id) {
     $sql = Sql::Delete($table)
       ->setWhere("`user_id` = ".$id);
-    return Database::Exec($sql);
+    Database::Exec($sql);
+    return Database::Result();
   }
 }
 
