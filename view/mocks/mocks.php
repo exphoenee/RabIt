e9c6c1aca5250f39f3440496c5a1a9bb7256bdd6
exphoenee <61277296+exphoenee@users.noreply.github.com>
2021-06-08 16:43:57 +0200

    Create MOCK for fast rendering

---
+++ view/mocks/mocks.php
@@ -0,0 +1,25 @@
+<?php
+
+  class Mocks {
+    public static $menu =
+    [
+      "home" =>
+      [
+        "text" => "Főoldal",
+        "link" => "home",
+      ],
+      "user" =>
+      [
+        "text" => "Felhasználók",
+        "link" => "users",
+      ],
+      "advert" =>
+      [
+        "text" => "Reklámok",
+        "link" => "adverts",
+      ],
+    ];
+
+  }
+
+?>
