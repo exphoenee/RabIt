39e23986def11498c29bedb2fd6f255fa3262ed4
exphoenee <61277296+exphoenee@users.noreply.github.com>
2021-06-08 19:29:28 +0200

    Created a TableView Class to render the tables faster

--- view/advertview.php
+++ view/advertview.php
@@ -1,8 +1,21 @@
 <?php
 class AdvertView {
 
-  public static function home() {
-    return '<div><h1>Advert</h1></div>';
+  public static function home($adverts) {
+
+    $html = '';
+
+    $html .= '<div><h1>Hirdetések</h1>';
+    $html .= View::renderEditorMenu();
+    $html .= self::renderAdvertTable($adverts);
+
+    $html .='</div>';
+    return $html;
+  }
+
+  public static function renderAdvertTable($adverts) {
+    $table = new TableView($adverts, true);
+    return $table->render();
   }
 }
 ?>
