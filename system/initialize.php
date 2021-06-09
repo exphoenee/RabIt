b42c04d422135f4bf6dc2acd4f149c29195f7894
exphoenee <61277296+exphoenee@users.noreply.github.com>
2021-06-09 15:07:36 +0200

    error reporting off

--- system/initialize.php
+++ system/initialize.php
@@ -3,9 +3,9 @@
 
   session_start();
   date_default_timezone_set("Europe/Budapest");
-  ini_set('display_errors', "on");
-  ini_set('error_reporting', E_ALL);
-  error_reporting(E_ALL);
+  ini_set('display_errors', "off");
+  ini_set('error_reporting', E_NOTICE);
+  error_reporting(E_NOTICE);
 
   Request::SubmitCheck();
   Database::Init(Config::$dbName, Config::$dbUser, Config::$dbPass);
